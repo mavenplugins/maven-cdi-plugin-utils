3abd92e4174aa7610d06b76a48e2a8bbed9ac045
Markus Hoffrogge <mhoffrogge@gmail.com>
2025-09-24 14:08:38 +0200

    Simplify WorkFlow reading and fix unit testing

--- src/test/java/com/itemis/maven/plugins/cdi/util/WorkflowValidatorTest.java
+++ src/test/java/com/itemis/maven/plugins/cdi/util/WorkflowValidatorTest.java
@@ -1,10 +1,12 @@
 package com.itemis.maven.plugins.cdi.util;
 
-import java.io.InputStream;
+import java.util.List;
 
+import org.apache.maven.plugin.MojoExecutionException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.itemis.maven.plugins.cdi.internal.util.workflow.WorkflowUtil;
 import com.itemis.maven.plugins.cdi.internal.util.workflow.WorkflowValidator;
 import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
@@ -14,18 +16,18 @@ public class WorkflowValidatorTest {
 
   @Test
   @DataProvider({ "try-finally", "try-finally_complex" })
-  public void testValidate(String workflowName) {
-    WorkflowValidator.validateSyntactically(getWorkflowAsStream(workflowName));
+  public void testValidate(String workflowName) throws MojoExecutionException {
+    WorkflowValidator.validateSyntactically(getTrimmedWorkflowLines(workflowName));
   }
 
   @Test(expected = RuntimeException.class)
   @DataProvider({ "invalid/try-finally_noTryBlockOpening", "invalid/try-finally_noTryBlockClosing",
       "invalid/try-finally_noFinallyBlock" })
-  public void testValidate_error(String workflowName) {
-    WorkflowValidator.validateSyntactically(getWorkflowAsStream(workflowName));
+  public void testValidate_error(String workflowName) throws MojoExecutionException {
+    WorkflowValidator.validateSyntactically(getTrimmedWorkflowLines(workflowName));
   }
 
-  private InputStream getWorkflowAsStream(String name) {
-    return Thread.currentThread().getContextClassLoader().getResourceAsStream("workflows/" + name);
+  private List<String> getTrimmedWorkflowLines(String name) throws MojoExecutionException {
+    return WorkflowUtil.getTrimmedWorkflowLines(WorkflowUtil.getResourceStream("workflows/" + name));
   }
 }
